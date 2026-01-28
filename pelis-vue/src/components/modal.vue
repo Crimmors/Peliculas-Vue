028df2f256317ce3fd7b4d57b63e82fda141a86e
Darío <lnq.carrerad.6c@gmail.com>
2026-01-28 12:18:03 -0500

    Peliculas en VUE

---
+++ pelis-vue/src/components/modal.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+export default function Modal({ isOpen, onClose, movie }) {
+ if (!isOpen || !movie) return null;}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
